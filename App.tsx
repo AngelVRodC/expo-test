d9b284b31da1c1f5c429fad3640ea056637251ca
Angel Rodriguez <angel@frubana.co>
2020-04-26 13:26:02 -0500

    redux connection

--- App.tsx
+++ App.tsx
@@ -1,42 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StyleSheet } from 'react-native';
-import { Router, Scene } from 'react-native-router-flux';
-import HomeScreen from './HomeScreen/HomeScreen';
-import { Container, Header, Title, Left, Icon, Right, Button, Body, Content,Text, Card, CardItem } from "native-base";
+import { appStore } from './src/@shared/store';
+import AppRRoutes from './src/routes/index';
 
  const App = ()  => {
   return (
-    <Container>
-    <Header>
-    <Left>
-      <Button
-        transparent
-        onPress={() => console.log('open')}>
-        <Icon name="menu" />
-      </Button>
-    </Left>
-    <Body>
-        <Title>HomeScreen</Title>
-    </Body>
-    <Right />
-  </Header>
-      <Router >
-      <Scene key="root">
-      <Scene key="Listado de Usuarios" component={HomeScreen} initial={true} hideNavBar={true} />
-      <Scene key="Crear Usuario" component={HomeScreen} title="PageTwo" hideNavBar={true} />
-      </Scene>
-    </Router>
-    </Container>
+    <Provider store={appStore}>
+      <AppRRoutes />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
